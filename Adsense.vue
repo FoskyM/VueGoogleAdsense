05ada20cf5ccc13f9d017f4db57dd4fd395bd89e
FoskyM <i@mouse123.cn>
2023-03-01 14:29:09 +0800

    Create Adsense.vue

---
+++ Adsense.vue
@@ -0,0 +1,42 @@
+<template>
+	<ins class="adsbygoogle"
+		:data-ad-client="adClient"
+		:data-ad-slot="adSlot"
+		:data-ad-format="adFormat"
+		:style="adStyle"
+		:data-full-width-responsive="responsive"></ins>
+</template>
+
+<script>
+export default {
+	name: 'Adsense',
+	props: {
+		adClient: {
+			type: String,
+			required: true
+		},
+		adSlot: {
+			type: String,
+			required: true
+		},
+		adFormat: {
+			type: String,
+			required: false,
+			default: 'auto'
+		},
+		adStyle: {
+			type: String,
+			required: false,
+			default: 'display: block'
+		},
+		responsive: {
+			type: String,
+			required: false,
+			default: 'false'
+		}
+	},
+	mounted() {
+		(window.adsbygoogle = window.adsbygoogle || []).push({})
+	}
+}
+</script>
